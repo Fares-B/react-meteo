f0b374b3abf0dbee612d07964210ab695bdec781
Fares.B <f_bourih@etu-webschoolfactory.fr>
2021-07-27 15:37:28 +0200

    add bootstrap dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Button variant="primary">Primary</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
