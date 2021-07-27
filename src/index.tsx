fdcbc6dc51ec9db57b67850353be790633f33e6b
Fares.B <f_bourih@etu-webschoolfactory.fr>
2021-07-27 15:45:10 +0200

    add react-router dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <Router>
+            {/* <Nav />
+            <Route exact path="/" component={Home} />
+            <Route path="/:city" component={City} /> */}
+        </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
