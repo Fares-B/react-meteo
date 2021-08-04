7f0d0f0afbc63e85201d6fedbd11fcebd12b87ee
Fares.B <f_bourih@etu-webschoolfactory.fr>
2021-08-04 14:06:39 +0200

    fix: change text of modal (delete city)

--- src/components/CardCity.tsx
+++ src/components/CardCity.tsx
@@ -59,9 +59,9 @@ const CardCity: React.FC<Props> = ({city, status = false}) => {
 
             <Modal show={showModal} onHide={()=> setShowModal(false)} className="text-primary">
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Suppression d'une ville</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>Êtes vous sûr de vouloir supprimer {city.properties.name} ?</Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleDeleteCity}>
                         supprimer
